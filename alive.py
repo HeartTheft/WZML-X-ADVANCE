11f7a330c83d37cf4858cb76461b010e2a72ebb6
kumarpk45 <133167314+kumarpk45@users.noreply.github.com>
2024-10-20 09:21:59 -0700

    Update alive.py

--- alive.py
+++ alive.py
@@ -3,14 +3,14 @@ from requests import get as rget
 from os import environ
 from logging import error as logerror
 
-BASE_URL = environ.get('BASE_URL_HK', None)
+BASE_URL = environ.get('BASE_URL_HK', 'http://server.hostingup.in/')
 try:
     if len(BASE_URL) == 0:
         raise TypeError
     BASE_URL = BASE_URL.rstrip("/")
 except TypeError:
     BASE_URL = None
-PORT = environ.get('PORT', '6060')
+PORT = environ.get('PORT', '8060')
 if PORT is not None and BASE_URL is not None:
     while True:
         try:
