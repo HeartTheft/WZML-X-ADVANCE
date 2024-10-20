6c03692707331c44dc407936596d50344b34de82
HeartTheft <110565010+HeartTheft@users.noreply.github.com>
2024-10-20 22:42:19 +0530

    Update alive.py

--- alive.py
+++ alive.py
@@ -10,7 +10,7 @@ try:
     BASE_URL = BASE_URL.rstrip("/")
 except TypeError:
     BASE_URL = None
-port = os.getenv("PORT", 8000)
+port = os.getenv("PORT", None)
 if PORT is not None and BASE_URL is not None:
     while True:
         try:
